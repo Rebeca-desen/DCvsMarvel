625215c70b91d8fe539f478d0ff7325e1c97ad41
Rebeca-desen <rebecarss01@gmail.com>
2022-01-17 11:28:18 -0300

    configurando para o banco

--- Models/Herois.cs
+++ Models/Herois.cs
@@ -2,19 +2,27 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace projectC.Models
 {
-    public class HeroisDCMarvel {
-        public int IdHeroisDCMarvel {get; set;}
-        public string Name {get; set;}
-        public string Company {get; set;}
-        public int Weaknesses { get; set;}
-        public int Powers {get; set;}
-        public int Life {get; set;}
+    public class HeroisDCMarvel
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+       
+        [BsonElement("Name")]
+        public string Name { get; set; }
+        public string Company { get; set; }
+        public int Weaknesses { get; set; }
+        public int Powers { get; set; }
+        public int Life { get; set; }
 
-        public HeroisDCMarvel(int idHeroisDCMarvel, string name, string company, int weaknesses, int powers, int life){
-            IdHeroisDCMarvel = idHeroisDCMarvel;
+        public HeroisDCMarvel(string id, string name, string company, int weaknesses, int powers, int life)
+        {
+            Id = id;
             Name = name;
             Company = company;
             Weaknesses = weaknesses;
