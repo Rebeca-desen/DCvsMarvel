4047bb5a79339297378e92395b3aa142e6178b18
Rebeca-desen <rebecarss01@gmail.com>
2022-01-17 14:11:12 -0300

    controller herois

--- Controllers/HeroisController.cs
+++ Controllers/HeroisController.cs
@@ -2,16 +2,77 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using projectC.Models;
+using projectC.Services;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 
 namespace projectC.Controllers
 {
-    public class HeroisController : Controller {
+    public class HeroisController : Controller
+    {
         //Get Herois
-        public IActionResult Index(){
-            return View();
+        private readonly HeroisServices _heroiService;
+
+        public HeroisController(HeroisServices heroiService)
+        {
+            _heroiService = heroiService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<HeroisDCMarvel>> Get() =>
+            _heroiService.Get();
+
+        [HttpGet("{id:length(24)}", Name = "GetBook")]
+        public ActionResult<HeroisDCMarvel> Get(string id)
+        {
+            var heroi = _heroiService.Get(id);
+
+            if (heroi == null)
+            {
+                return NotFound();
+            }
+
+            return heroi;
+        }
+
+        [HttpPost]
+        public ActionResult<HeroisDCMarvel> Create(HeroisDCMarvel heroi)
+        {
+            _heroiService.Create(heroi);
+
+            return CreatedAtRoute("GetHeroi", new { id = heroi.Id.ToString() }, heroi);
+        }
+
+        [HttpPut("{id:length(24)}")]
+        public IActionResult Update(string id, HeroisDCMarvel heroisIn)
+        {
+            var heroi = _heroiService.Get(id);
+
+            if (heroi == null)
+            {
+                return NotFound();
+            }
+
+            _heroiService.Update(id, heroisIn);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        public IActionResult Delete(string id)
+        {
+            var heroi = _heroiService.Get(id);
+
+            if (heroi == null)
+            {
+                return NotFound();
+            }
+
+            _heroiService.Remove(heroi.Id);
+
+            return NoContent();
         }
     }
 }
