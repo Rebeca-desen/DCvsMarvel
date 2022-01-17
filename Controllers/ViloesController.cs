5ec6fec2ad3c9fef79152e955c7c175158e7e06c
Rebeca-desen <rebecarss01@gmail.com>
2022-01-17 14:35:04 -0300

    controller vilao

--- Controllers/ViloesController.cs
+++ Controllers/ViloesController.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using projectC.Models;
+using projectC.Services;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -9,9 +11,67 @@ using System.Diagnostics;
 namespace projectC.Controllers
 {
     public class ViloesController : Controller {
-        //Get Viloes
-        public IActionResult Index(){
-            return View();
+                private readonly ViloesServices _viloesService;
+
+        public ViloesController(ViloesServices vilaoService)
+        {
+            _viloesService = vilaoService;
         }
+
+        [HttpGet]
+        public ActionResult<List<Viloes>> Get() =>
+            _viloesService.Get();
+
+        [HttpGet("{id:length(24)}", Name = "GetVilao")]
+        public ActionResult<Viloes> Get(string id)
+        {
+            var vilao = _viloesService.Get(id);
+
+            if (vilao == null)
+            {
+                return NotFound();
+            }
+
+            return vilao;
+        }
+
+        [HttpPost]
+        public ActionResult<Viloes> Create(Viloes vilao)
+        {
+            _viloesService.Create(vilao);
+
+            return CreatedAtRoute("Getvilao", new { id = vilao.Id.ToString() }, vilao);
+        }
+
+        [HttpPut("{id:length(24)}")]
+        public IActionResult Update(string id, Viloes viloesIn)
+        {
+            var vilao = _viloesService.Get(id);
+
+            if (vilao == null)
+            {
+                return NotFound();
+            }
+
+            _viloesService.Update(id, viloesIn);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        public IActionResult Delete(string id)
+        {
+            var vilao = _viloesService.Get(id);
+
+            if (vilao == null)
+            {
+                return NotFound();
+            }
+
+            _viloesService.Remove(vilao.Id);
+
+            return NoContent();
+        }
+      
     }
 }
